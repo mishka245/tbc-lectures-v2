01092e4d9d9ed866286b605201f9bf3c1fd3e2bc
Mikheil Lomidze <lomidzemikheili@gmail.com>
2025-01-10 09:10:51 +0400

    Add lecture 28.

---
+++ lecture_28/example_3.py
@@ -0,0 +1,41 @@
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.orm import declarative_base, sessionmaker
+
+# Create an engine
+# pip install sqlalchemy psycopg2
+# or
+# poetry add sqlalchemy psycopg2
+
+#docker run --name tapi-postgres -e POSTGRES_USER=tapi-admin -e POSTGRES_PASSWORD=mystrongpassword -e POSTGRES_DB=tapi-db -p 5432:5432 -d postgres
+engine = create_engine('postgresql://tapi-admin:mystrongpassword@localhost:5432/tapi-db')
+
+# Create a connection
+connection = engine.connect()
+
+Base = declarative_base()
+
+Session = sessionmaker(bind=engine)
+
+
+class Student(Base):
+    __tablename__ = 'students'
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String, nullable=False)
+    age = Column(Integer, nullable=True)
+    city = Column(String, nullable=True)
+
+# create tables, if not exists
+# Can not detect changes
+Base.metadata.create_all(engine)
+
+
+
+def main():
+    with Session.begin() as session:
+        student_1 = Student(name="John Doe", age=25, city="New York")
+        session.add(student_1)
+
+if __name__ == "__main__":
+    main()
+
